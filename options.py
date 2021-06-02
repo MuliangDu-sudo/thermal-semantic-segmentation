0654e60a1469dbcea623b9d8a06fe1b1c3f3c554
MuliangDu-sudo <m.du@student.tue.nl>
2021-06-02 03:08:08 +0200

    add options for semantic segmentation train

--- options.py
+++ options.py
@@ -16,3 +16,13 @@ def translation_parse():
     parser.add_argument('-checkpoint_name', type=str, default='without_sem.pth',
                         help='the name of trained model.')
     return parser
+
+
+def seg_parse():
+    parser = argparse.ArgumentParser(description='segmentation options')
+    parser.add_argument('-load_model', default=True, help='train with pretrained model.')
+    parser.add_argument('-epochs', default=10, help='number of epochs to train.')
+    parser.add_argument('-batch_size', type=int, default=1, help='batch size for translation.')
+    parser.add_argument('-checkpoint_name', type=str, default='semantic_segmentation.pth',
+                        help='the name of trained model.')
+    return parser
