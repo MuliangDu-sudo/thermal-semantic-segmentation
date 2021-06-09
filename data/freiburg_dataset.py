466746528eb426f9ff05a4d5ce5057409942ec3b
MuliangDu-sudo <m.du@student.tue.nl>
2021-06-09 02:45:51 +0200

    add freiburg_dataset

---
+++ data/freiburg_dataset.py
@@ -0,0 +1,75 @@
+import os
+from .base_dataset import BaseDataset
+from utils import freiburg_txt
+from PIL import Image
+import torch
+import numpy as np
+from torch.utils import data
+
+
+class Freiburg(data.Dataset):
+
+    def __init__(self, root, split, domain, transforms, with_label, **kwargs):
+        """
+        :param root: str. root path to the dataset.
+        :param split: str. train or test.
+        :param domain: str. RGB or IR.
+        :param transforms: transforms pipeline.
+        :param kwargs: others.
+        :param with_label: bool. whether returns label
+        """
+
+        assert split in ['train', 'test']
+
+        data_list_file = os.path.join(root, "image_list", "{}_{}_data.txt".format(split, domain))
+        label_list_file = os.path.join(root, "image_list", "{}_{}_label.txt".format(split, domain))
+        if not (os.path.exists(data_list_file) and os.path.exists(label_list_file)):
+            freiburg_txt(root, split, domain)
+        self.data_list = self.parse_file(data_list_file)
+        self.label_list = self.parse_file(label_list_file)
+        self.split = split
+        self.domain = domain
+        self.transforms = transforms
+        self.with_label = with_label
+
+    def parse_file(self, file_name):
+        """Parse file to image list
+
+        Args:
+            file_name (str): The path of data file
+
+        Returns:
+            List of image path
+        """
+
+        with open(file_name, "r") as f:
+            data_list = [line.strip() for line in f.readlines()]
+        return data_list
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, item):
+
+        image_name = self.data_list[item]
+        label_name = self.label_list[item]
+        if self.domain == 'IR':
+            image = Image.open(os.path.join(image_name))
+        elif self.domain == 'RGB':
+            image = Image.open(os.path.join(image_name)).convert('RGB')
+        else:
+            raise ValueError('Not a valid domain.')
+
+        if self.with_label:
+            label = np.load(label_name)
+            label = Image.fromarray(label)
+            image, label = self.transforms(image, label)
+            return image, label
+        else:
+            image = self.transforms(image)
+            return image
+
+
+
+
+
