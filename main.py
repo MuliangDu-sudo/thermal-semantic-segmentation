76fb107c19859c434877088ff3f2d0bf431485fa
MuliangDu-sudo <m.du@student.tue.nl>
2021-06-24 00:04:52 +0200

    add contour loss

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import torch
 from utils import loss, ImagePool
-from utils import transforms as T
+from utils import transforms as TT
+from utils import triple_transforms as TTT
 from data import Cityscapes, TrainTDataset, Freiburg
 from torch.utils.data import DataLoader
-from models import discriminators, generators, semantic_segmentation_models, thermal_semantic_segmentation_models
+from models import discriminators, generators, semantic_segmentation_models, thermal_semantic_segmentation_models, Canny
 from itertools import chain
 from train import train
 from options import train_parse
-from torchvision import transforms as TT
+from torchvision import transforms as T
 import visdom
 import os
 from PIL import ImageFile
@@ -20,28 +21,39 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MODEL_ROOT_PATH = './checkpoints'
 
 
-
 def main(args):
-    source_train_transform = T.Compose([
+    single_transform = T.Compose([
         T.RandomResizedCrop(size=(256, 512), ratio=(1.5, 8 / 3.), scale=(0.5, 1.)),
-        # it return an image of size 256x512
         T.RandomHorizontalFlip(),
         T.ToTensor(),
-        T.Normalize(args.normalize, args.normalize)
+        T.Normalize((0.5,), (0.5,))
     ])
 
-    target_train_transform = TT.Compose([
+    double_transform = TT.Compose([
         TT.RandomResizedCrop(size=(256, 512), ratio=(1.5, 8 / 3.), scale=(0.5, 1.)),
+        # it return an image of size 256x512
         TT.RandomHorizontalFlip(),
         TT.ToTensor(),
-        TT.Normalize((0.5,), (0.5,))
+        TT.Normalize(args.normalize, args.normalize)
     ])
+
+    # triple transform only used for items with contour original image
+    triple_transform = TTT.Compose([
+        TTT.RandomResizedCrop(size=(256, 512), ratio=(1.5, 8 / 3.), scale=(0.5, 1.)),
+        # it return an image of size 256x512
+        TTT.RandomHorizontalFlip(),
+        TTT.ToTensor(),
+        TTT.Normalize(args.normalize, args.normalize)
+    ])
+
+    source_train_transform = triple_transform
+    target_train_transform = triple_transform
     # data loading
     if args.source_dataset == 'Cityscapes':
         source_dataset = Cityscapes('datasets/source_dataset', transforms=source_train_transform)
     elif args.source_dataset == 'freiburg_rgb':
         source_dataset = Freiburg('datasets/freiburg', split='train', domain='RGB', transforms=source_train_transform,
-                                  with_label=True, grayscale=args.grayscale)
+                                  with_label=True, with_contour=args.with_contour, grayscale=args.grayscale)
     else:
         raise ValueError('source dataset does not exist.')
 
@@ -49,7 +61,7 @@ def main(args):
         target_dataset = TrainTDataset('datasets/target_dataset', transforms=target_train_transform)
     elif args.target_dataset == 'freiburg_ir':
         target_dataset = Freiburg('datasets/freiburg', split='train', domain='IR', transforms=target_train_transform,
-                                  with_label=False)
+                                  with_label=False, with_contour=args.with_contour)
     else:
         raise ValueError('target dataset does not exist.')
 
@@ -65,6 +77,7 @@ def main(args):
     net_d_t = discriminators.NLayerDiscriminator(input_nc=args.t2s_input_nc).to(device)
     net_seg_s = semantic_segmentation_models.deeplabv2_resnet101().to(device)
     net_seg_t = thermal_semantic_segmentation_models.deeplabv2_resnet101_thermal(pretrained_backbone=False).to(device)
+    canny = Canny(device, threshold=3, batch_size=args.batch_size).to(device)
 
     restart_epoch = 0
     if args.load_model:
@@ -91,14 +104,15 @@ def main(args):
     gan_loss_func = loss.LeastSquaresGenerativeAdversarialLoss()
     cycle_loss_func = torch.nn.L1Loss()
     identity_loss_func = torch.nn.L1Loss()
+    contour_loss_func = torch.nn.L1Loss()
     sem_loss_func = loss.SemanticConsistency().to(device)
     loss_dict = {'g_s2t': [], 'g_t2s': [], 'd_s': [], 'd_t': [], 'cycle_s': [], 'cycle_t': []}
     epoch_counter_ratio = []
     print("--------START TRAINING--------")
     for epoch in range(restart_epoch, restart_epoch+args.num_epoch):
         print("--------EPOCH {}--------".format(epoch))
-        train(args, train_source_loader, train_target_loader, net_g_s2t, net_g_t2s, net_d_s, net_d_t, net_seg_s, net_seg_t,
-              gan_loss_func, cycle_loss_func, identity_loss_func, sem_loss_func, optimizer_g, optimizer_d, fake_s_pool,
+        train(args, train_source_loader, train_target_loader, net_g_s2t, net_g_t2s, net_d_s, net_d_t, canny, net_seg_s, net_seg_t,
+              gan_loss_func, cycle_loss_func, identity_loss_func, sem_loss_func, contour_loss_func, optimizer_g, optimizer_d, fake_s_pool,
               fake_t_pool, device, epoch, visualizer, loss_dict, epoch_counter_ratio)
 
         torch.save({
