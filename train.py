9e90d3db35488c949c0a3159542eb72b1f0592b8
Muliang Du <m.du@student.tue.nl>
2021-07-11 01:59:28 +0200

    change contour extraction setting

--- train.py
+++ train.py
@@ -178,11 +178,11 @@ def train(args, s_data, t_data, g_s2t, g_t2s, d_s, d_t, canny, sem_net_s, sem_ne
             # below is for gray2ir
             contour_s_ori = s[0].to(device)
             contour_t_ori = t[0].to(device)
-            contour_real_s = canny['thermal'](contour_s_ori).detach()
+            contour_real_s = canny['rgb'](contour_s_ori).detach()
             contour_real_t = canny['thermal'](contour_t_ori).detach()
             contour_fake_t = canny['thermal'](fake_t).detach()
             gray_fake_s = fake_s
-            contour_fake_s = canny['thermal'](gray_fake_s).detach()
+            contour_fake_s = canny['rgb'](gray_fake_s).detach()
 
             loss_contour_s2t = contour_loss_func(contour_real_s, contour_fake_t)
             loss_contour_t2s = contour_loss_func(contour_real_t, contour_fake_s)
